2b026ffb3aea98e75338212096b840ec8f3c67a3
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-08-14 14:59:00 +0600

    Category tag added to my project

--- blog/post/admin.py
+++ blog/post/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Tag
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'category_slug')
+    search_fields = ('category_name', 'category_slug')
+
+admin.site.register(Category, CategoryAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag_name', 'tag_slug')
+    search_fields = ('tag_name', 'tag_slug')
+
+admin.site.register(Tag, TagAdmin)
--- blog/post/models.py
+++ blog/post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=200, default='')
+    category_slug = models.SlugField(max_length=200, unique=True, default='')
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.category_name
+    
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=200, default='')
+    tag_slug = models.SlugField(max_length=200, unique=True, default='')
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.tag_name
+    
+
