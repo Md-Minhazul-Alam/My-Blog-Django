ddf199b649a3b4759bc6408dba386cbfd53e75d5
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-08-16 00:26:46 +0600

    Auto Slug system added into tag and category

--- blog/post/models.py
+++ blog/post/models.py
@@ -4,19 +4,53 @@ from django.utils.text import slugify
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=200, default='')
-    category_slug = models.SlugField(max_length=200, unique=True, default='')
+    category_slug = models.SlugField(max_length=200, unique=True, blank=True, default='')
     is_active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.category_name
     
+    def save(self, *args, **kwargs):
+        # If slug is empty, generate from tag
+        if not self.category_slug:
+            base_slug = slugify(self.category_name)
+        else:
+            base_slug = slugify(self.category_slug)
+
+        # Ensure uniqueness
+        slug = base_slug
+        num = 1
+        while Category.objects.filter(category_slug=slug).exclude(pk=self.pk).exists():
+            slug = f"{base_slug}-{num}"
+            num += 1
+
+        self.category_slug = slug
+        super().save(*args, **kwargs)
+    
 class Tag(models.Model):
     tag_name = models.CharField(max_length=200, default='')
-    tag_slug = models.SlugField(max_length=200, unique=True, default='')
+    tag_slug = models.SlugField(max_length=200, unique=True, blank=True, default='')
     is_active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.tag_name
+    
+    def save(self, *args, **kwargs):
+        # If slug is empty, generate from tag
+        if not self.tag_slug:
+            base_slug = slugify(self.tag_name)
+        else:
+            base_slug = slugify(self.tag_slug)
+
+        # Ensure uniqueness
+        slug = base_slug
+        num = 1
+        while Tag.objects.filter(tag_slug=slug).exclude(pk=self.pk).exists():
+            slug = f"{base_slug}-{num}"
+            num += 1
+
+        self.tag_slug = slug
+        super().save(*args, **kwargs)
     
 class Blog(models.Model):
     blog_name = models.CharField(max_length=200, default='')
