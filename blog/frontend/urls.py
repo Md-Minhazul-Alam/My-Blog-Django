2b06212f70c6e25baa841e0ccc4660d782d54c36
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-08-16 00:16:05 +0600

    Category Menus converted into dynamic URL

--- blog/frontend/urls.py
+++ blog/frontend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from frontend import views
 
 urlpatterns = [
-    path('', views.HomePage, name='home')
+    path('', views.HomePage, name='home'),
+    path('category/<slug:category_slug>', views.CategoryPage, name='categoryPage')
 ]
--- blog/frontend/views.py
+++ blog/frontend/views.py
@@ -1,7 +1,18 @@
+from post.models import Category
 from django.shortcuts import render
 import requests
 
 # Create your views here.
 def HomePage(request):
+    category = Category.objects.all()
+    
+    return render(request, "pages/home.html", {
+        'categories': category, 
+    })
 
-    return render(request, "pages/home.html")
+def CategoryPage(request, category_slug):
+    category = Category.objects.all()
+
+    return render(request, "pages/category.html", {
+        'categories': category,
+    })
