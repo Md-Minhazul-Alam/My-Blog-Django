d0491b2baf773e206c5c6756ec75e6779204f1d1
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-08-16 15:57:47 +0600

    Logic build to get category blogs

--- blog/frontend/views.py
+++ blog/frontend/views.py
@@ -1,33 +1,38 @@
-from post.models import Category, Social
+from post.models import Category, Social, Blog
 from django.shortcuts import render
 import requests 
 from websitesetting.models import Setting
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def HomePage(request):
     # All Categories
-    category = Category.objects.all()
+    categoryMenu = Category.objects.all()
     #  Social
     social = Social.objects.all()
     # Website Setting
     setting = Setting.objects.latest('id')
     
     return render(request, "pages/home.html", {
-        'categories': category,
+        'categories': categoryMenu,
         'socials': social, 
         'settings': setting,
     })
     
 def CategoryPage(request, category_slug):
     # All Categories
-    category = Category.objects.all()
+    categoryMenu = Category.objects.all()
     #  Social
     social = Social.objects.all()
     # Website Setting
     setting = Setting.objects.latest('id')
+    # Category Blog
+    category = get_object_or_404(Category, category_slug=category_slug, is_active=True)
+    blogs = Blog.objects.filter(category=category, is_active=True)
 
     return render(request, "pages/category.html", {
-        'categories': category,
+        'categories': categoryMenu,
         'socials': social,
         'settings': setting,
+        'blogs': blogs,
     })
