1aff70a1721205a99ca3e273334bf146b6cf70fe
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-08-17 15:34:02 +0600

    Worked on pages dynamic functions

--- blog/frontend/urls.py
+++ blog/frontend/urls.py
@@ -3,5 +3,6 @@ from frontend import views
 
 urlpatterns = [
     path('', views.HomePage, name='home'),
-    path('category/<slug:category_slug>', views.CategoryPage, name='categoryPage')
+    path('category/<slug:category_slug>', views.CategoryPage, name='categoryPage'),
+    path('page/<slug:page_slug>', views.WebsitePage, name='page'),
 ]
--- blog/frontend/views.py
+++ blog/frontend/views.py
@@ -1,4 +1,4 @@
-from post.models import Category, Social, Blog
+from post.models import Category, Social, Blog, Page
 from django.shortcuts import render
 import requests 
 from websitesetting.models import Setting
@@ -12,11 +12,14 @@ def HomePage(request):
     social = Social.objects.all()
     # Website Setting
     setting = Setting.objects.latest('id')
+    # Page
+    page = Page.objects.all
     
     return render(request, "pages/home.html", {
         'categories': categoryMenu,
         'socials': social, 
         'settings': setting,
+        'pages': page,
     })
     
 def CategoryPage(request, category_slug):
@@ -29,10 +32,35 @@ def CategoryPage(request, category_slug):
     # Category Blog
     category = get_object_or_404(Category, category_slug=category_slug, is_active=True)
     blogs = Blog.objects.filter(category=category, is_active=True)
+    # Page
+    page = Page.objects.all
 
     return render(request, "pages/category.html", {
         'categories': categoryMenu,
         'socials': social,
         'settings': setting,
         'blogs': blogs,
+        'pages': page,
+    })
+
+# Page
+def WebsitePage(request, page_slug):
+    # All Categories
+    categoryMenu = Category.objects.all()
+    #  Social
+    social = Social.objects.all()
+    # Website Setting
+    setting = Setting.objects.latest('id')
+    # Page
+    pageMenu = Page.objects.all
+    # Page
+    page = get_object_or_404(Page, page_slug=page_slug, is_active=True)
+    pageDetails = Page.objects.filter(page=page, is_active=True)
+    
+    return render(request, "pages/page.html", {
+        'categories': categoryMenu,
+        'socials': social, 
+        'settings': setting,
+        'pages': pageMenu,
+        'pageInfo': pageDetails
     })
