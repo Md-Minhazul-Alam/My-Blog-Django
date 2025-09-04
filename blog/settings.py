825b632d23435a5bb5432947765a379e0a88ca1d
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-04 16:58:50 +0600

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -157,3 +157,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cache settings
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache'), 
+    }
+}
