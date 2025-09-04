8b6094c937b0dc34a100538f605e180de884ecb0
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-09-04 21:30:29 +0600

    Update urls.py

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 from frontend import views
 
 urlpatterns = [
-    path('', views.HomePage, name='home'),
-    path('category/<slug:category_slug>', views.CategoryPage, name='categoryPage'),
-    path('blog-details/<slug:blog_slug>', views.blogDetails, name='blogDetails'),
-    path('page/<slug:page_slug>', views.WebsitePage, name='page'),
-    path('search-blogs/', views.searchList, name='searchList'), 
+    path('', views.home_page, name='home'),
+    path('category/<slug:category_slug>', views.category_page, name='categoryPage'),
+    path('blog-details/<slug:blog_slug>', views.blog_details, name='blogDetails'),
+    path('page/<slug:page_slug>', views.website_page, name='page'),
+    path('search-blogs/', views.search_list, name='searchList'), 
     # Comments Path
     path('blog/<slug:blog_slug>/add-comment/', views.add_comment, name='add_comment'),
     path('verify-comment-owner/', views.verify_comment_owner, name='verify_comment_owner'),
     path('edit-comment/', views.edit_comment, name='edit_comment'),
     path('delete-comment/', views.delete_comment, name='delete_comment'),
-    
 ]
