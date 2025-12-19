29bb1f88c5a52baf0cf44fff0a1645b88cbe4258
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-12-19 11:43:03 +0600

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import cloudinary
-import environ
-
 from dotenv import load_dotenv
 import dj_database_url
 
@@ -37,7 +35,7 @@ SECRET_KEY = 'django-insecure-gy(v$6nn2qc00p+f70g5swsr%@0u+t=z)sb2@6(1ax$g0l2ry8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://minhazulmyblog-2071031bcb58.herokuapp.com']
+ALLOWED_HOSTS = ['minhazulmyblog-2071031bcb58.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://minhazulmyblog-2071031bcb58.herokuapp.com"
