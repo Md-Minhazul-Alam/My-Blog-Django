3d533703c706fc20069b4f2cef005b37c0039d3d
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-12-19 12:13:19 +0600

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -39,11 +39,13 @@ SECRET_KEY = 'django-insecure-gy(v$6nn2qc00p+f70g5swsr%@0u+t=z)sb2@6(1ax$g0l2ry8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS", "127.0.0.1,localhost,minhazulmyblog-2071031bcb58.herokuapp.com"
+).split(",")
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "https://minhazulmyblog-2071031bcb58.herokuapp.com"
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    "https://minhazulmyblog-2071031bcb58.herokuapp.com"
+]
 
 # Application definition
 
