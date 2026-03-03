3a33a0cf83caf1639d3a0fb737234adae610509f
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2026-03-03 19:10:28 +0600

    Removed Unnecessary Local .env Configuration

--- blog/settings.py
+++ blog/settings.py
@@ -12,19 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import cloudinary
-import environ
-from dotenv import load_dotenv
 import dj_database_url
-
-load_dotenv()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Initialize environment variables
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
