faee2aea13407e2c37be06c348e3e749e336cee9
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2026-03-03 18:37:17 +0600

    Reoved SECRET_KEY & Moved Heroku Env

--- blog/settings.py
+++ blog/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gy(v$6nn2qc00p+f70g5swsr%@0u+t=z)sb2@6(1ax$g0l2ry8'
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
