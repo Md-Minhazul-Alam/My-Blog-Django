ff47a0bf58417914e793d2822319cd05c1df1056
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2026-03-06 09:54:26 +0600

    Optimize urls.py for PEP8 & validated

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,13 +3,17 @@ from frontend import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('category/<slug:category_slug>', views.category_page, name='categoryPage'),
-    path('blog-details/<slug:blog_slug>', views.blog_details, name='blogDetails'),
+    path('category/<slug:category_slug>',
+         views.category_page, name='categoryPage'),
+    path('blog-details/<slug:blog_slug>',
+         views.blog_details, name='blogDetails'),
     path('page/<slug:page_slug>', views.website_page, name='page'),
-    path('search-blogs/', views.search_list, name='searchList'), 
+    path('search-blogs/', views.search_list, name='searchList'),
     # Comments Path
-    path('blog/<slug:blog_slug>/add-comment/', views.add_comment, name='add_comment'),
-    path('verify-comment-owner/', views.verify_comment_owner, name='verify_comment_owner'),
+    path('blog/<slug:blog_slug>/add-comment/',
+         views.add_comment, name='add_comment'),
+    path('verify-comment-owner/',
+         views.verify_comment_owner, name='verify_comment_owner'),
     path('edit-comment/', views.edit_comment, name='edit_comment'),
     path('delete-comment/', views.delete_comment, name='delete_comment'),
 ]
