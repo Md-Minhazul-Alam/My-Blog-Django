4a14d0e4d233569f1862fa9e8797ecd3be4d37f7
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2026-03-08 18:32:45 +0600

    Validate pep8ci for websitesetting admin & added Testing.nd

--- websitesetting/admin.py
+++ websitesetting/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from websitesetting.models import Setting
 
+
 # Register Website Setting
 class WebsiteSettingAdmin(admin.ModelAdmin):
     list_display = ('site_name', 'site_meta_keywords')
